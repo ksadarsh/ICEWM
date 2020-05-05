e056d4e0214b3f732c4fc37b538854256c77c152
ksadarsh <59079003+ksadarsh@users.noreply.github.com>
2020-05-05 22:04:25 +0530

    Add files via upload

---
+++ newinstall.sh
@@ -0,0 +1,5 @@
+sudo apt install pcmanfm cbatticon gtk2-engines-murrine software-properties-common gnome-font-viewer gnome-disk-utility numlockx wmctrl gdebi synaptic git curl wget rofi dmenu lxappearance geany cmake dunst build-essential htop file-roller compton lxpolkit dkms bleachbit gvfs gvfs-backends volumeicon-alsa alsa-utils pavucontrol yad acpid upower 
+sudo apt install linux-headers-$(uname -r)
+git clone https://github.com/ksadarsh/JWM.git
+git clone https://github.com/ksadarsh/Debian.git
+git clone https://github.com/ksadarsh/Icewm.git
